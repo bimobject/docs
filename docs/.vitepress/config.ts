3065bd9b57300bb709ff1f00c10f367df8635f83
Daniel Olausson <daniel.olsusson@eando.se>
2023-10-19 14:32:50 +0200

    first deploy

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "EandoX",
+  description: "Docs for Eando X",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+          { text: 'Products', link: '/getting-started' },
+          { text: 'test', link: '/test' },
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ docs/test.md
@@ -0,0 +1,12 @@
+[homepage](https://eanox.carbonzero.com "homepage")
+
+
+
+[eanox.carbonzero.com](https://eanox.carbonzero.com)
+
+<div class="dark-frame">
+  <img src="./test.jpg" alt="An image">
+</div>
+
+
+![An image](./test.jpg)
