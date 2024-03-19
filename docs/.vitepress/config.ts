744328e52533b9710878acb663afd56e1c02b7a8
Michael Cartner <michael@cartner.se>
2024-03-19 13:22:38 +0100

    updated baseURL

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "EandoX",
   description: "Docs for Eando X",
-  base: "/docs/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
