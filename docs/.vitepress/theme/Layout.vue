05eceb6dcb435840e1ec3b22c4c48c6dfa861078
kotte <konstantin.aistre@gmail.com>
2025-06-12 09:21:37 +0200

    Feat: Add Mouseflow tracking to docs

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,17 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    window._mfq = window._mfq || [];
+
+    var mf = document.createElement("script");
+    mf.type = "text/javascript"; mf.defer = true;
+    mf.src = "//cdn.mouseflow.com/projects/a436e030-c54e-48a6-b7a9-9eb9a5a42729.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+})
+</script>
+
+<template>
+    <DefaultTheme.Layout />
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import AiContent from '../../components/AiContent.vue';
+import Layout from './Layout.vue';
 import './custom.css';
 
 export default {
@@ -8,4 +9,5 @@ export default {
   enhanceApp({ app }) {
     app.component('AiContent', AiContent);
   },
+  Layout,
 } satisfies Theme;
