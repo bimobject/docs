fc59f731ebeb8b8b86e04cba09180ff8c851d15e
kotte <konstantin.aistre@gmail.com>
2025-06-19 09:18:15 +0200

    Feat: disable tracking on localhost, in-dashboard and minidocs

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -3,12 +3,22 @@ import DefaultTheme from 'vitepress/theme'
 import { onMounted } from 'vue'
 
 onMounted(() => {
+    const untrackedPaths = ['/in-dashboard', '/minidocs/'];
+
+    if (
+        untrackedPaths.some(path => window.location.pathname.includes(path)) ||
+        window.location.hostname === 'localhost'
+    ) {
+        return
+    }
+
+
     window._mfq = window._mfq || [];
 
-    var mf = document.createElement("script");
-    mf.type = "text/javascript"; mf.defer = true;
-    mf.src = "//cdn.mouseflow.com/projects/b24688bd-712f-4445-bd16-b89ca6de37c5.js";
-    document.getElementsByTagName("head")[0].appendChild(mf);
+    var mf = document.createElement('script');
+    mf.type = 'text/javascript'; mf.defer = true;
+    mf.src = '//cdn.mouseflow.com/projects/b24688bd-712f-4445-bd16-b89ca6de37c5.js';
+    document.getElementsByTagName('head')[0].appendChild(mf);
 })
 </script>
 
